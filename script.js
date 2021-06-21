a98a9ea057fd930ee383edcd1b6bcd999780a41d
Caio Augusto <caioamfr@gmail.com>
2021-06-21 16:29:06 -0300

    Criado acordião

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const tabMenu = document.querySelectorAll(".animais-lista li");
-const tabContent = document.querySelectorAll(".animais-descricao section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".animais-lista li");
+  const tabContent = document.querySelectorAll(".animais-descricao section");
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("active");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("active");
 
-  function activeTab(index) {
-    tabContent.forEach(el => el.classList.remove("active"));
-    tabContent[index].classList.add("active");
+    function activeTab(index) {
+      tabContent.forEach(el => el.classList.remove("active"));
+      tabContent[index].classList.add("active");
+    }
+
+    tabMenu.forEach((el, index) =>
+      el.addEventListener("click", () => activeTab(index))
+    );
+  }
+}
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".faq-lista dt");
+  accordionList[0].nextElementSibling.classList.add("active");
+
+  function activeAccordion() {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("active");
   }
 
-  tabMenu.forEach((el, index) =>
-    el.addEventListener("click", () => activeTab(index))
-  );
+  accordionList.forEach(el => {
+    el.addEventListener("click", activeAccordion);
+  });
 }
+initAccordion();
