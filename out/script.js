cb733274c9f5f15675fde89d67c14255adca0796
Caio Augusto <caioamfr@gmail.com>
2021-08-19 17:26:44 -0300

    Remoção de arquivo não utilizado

--- out/script.js
+++ out/script.js
@@ -9,7 +9,11 @@ import { MenuMobile } from "./modules/menuMobile.js";
 new SmoothScroll('.menu a[href^="#"]', SMOOTH_SCROLL_OPTIONS);
 new Accordion(".faq-lista dt");
 new TabNav(".animais-lista li", ".animais-descricao section");
-new Modal('[data-modal="open"]', '[data-modal="close"]', '[data-modal="container"]');
+new Modal(
+  '[data-modal="open"]',
+  '[data-modal="close"]',
+  '[data-modal="container"]'
+);
 new Tooltip("[data-tooltip]");
 new DropdownMenu("[data-dropdown]");
 new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
